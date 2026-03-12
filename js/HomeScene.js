47f8ad3f08fc92d2d5b5be44e5dbbe16fc499b15
BRAINMELTERhg <BRAINMELTER.hg@gmail.com>
2026-03-11 23:31:27 -0700

    so close 8

--- js/HomeScene.js
+++ js/HomeScene.js
@@ -92,7 +92,7 @@ unlockDiv.addEventListener("pointerdown", () => {
         // Music //hhmmmm
         this.music = this.sound.add("music", { loop: true });
         this.musicOn = false;
-        this.musicText = this.add.text(-60, 2, "music off", { font: "24px Arial", color: "#ffffff" })
+        this.musicText = this.add.text(-60, 700, "music off", { font: "24px Arial", color: "#ffffff" })
             .setScrollFactor(0)
             .setDepth(1000)
             .setInteractive();
